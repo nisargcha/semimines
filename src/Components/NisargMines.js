b35ff7e3923f7a8abc361158da99554a1da7156e
nivi3026 <nivi300426@gmail.com>
2024-05-20 16:39:30 +0530

    Bomb

--- src/Components/NisargMines.js
+++ src/Components/NisargMines.js
@@ -23,6 +23,7 @@ export default function NisargMine() {
                 newGrid[row][column].content = 'bomb';
                 minecnt++;
             }
+        console.log("Bomb" + row + column);
 
         }
         setGrid(newGrid); 
