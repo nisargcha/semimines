a0d536bdbf8222cb93e5822267661c422f8b8e6e
nivi3026 <nivi300426@gmail.com>
2024-05-20 16:42:12 +0530

    Final Commit

--- src/Components/NisargMines.js
+++ src/Components/NisargMines.js
@@ -23,7 +23,6 @@ export default function NisargMine() {
                 newGrid[row][column].content = 'bomb';
                 minecnt++;
             }
-        console.log("Bomb" + row + column);
 
         }
         setGrid(newGrid); 
